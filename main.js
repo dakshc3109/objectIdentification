d575a6dd64c41055226d845ec008b4a86c3f16d4
Daksh Chaudhary <78908644+dakshc3109@users.noreply.github.com>
2021-11-09 16:56:39 +0530

    //added main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var img = "";
+var status = ""; 
 
 function setup(){
     canvas = createCanvas(640, 420);
-    canvas.center();
+    //canvas.center();
+    canvas.position(330, 120);;
+    objetDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting object";
 }
 
 function preload(){
@@ -16,4 +20,25 @@ function draw(){
     text("Dog", 130, 75);
     noFill();
     rect(120, 60, 250, 300);
+
+    stroke("#ff0000");
+    fill("#ff0000");
+    text("Cat", 320, 120);
+    noFill();
+    rect(310, 110, 250, 300);
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    status = true;
+    objetDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error();
+    }
+    else{
+        console.log(results);
+    }
 }
